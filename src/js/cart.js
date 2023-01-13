591f81d6824506d91976c4a30ca4f8a7e7ad1efe
mlutz06 <madison.lutz@sentinelofsafety.com>
2023-01-13 12:27:09 -0700

    fix add to cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,10 +1,12 @@
-import { getLocalStorage } from "./utils.mjs";
+import { getLocalStorage } from './utils.mjs';
 
 function renderCartContents() {
-  const cartItems = getLocalStorage("so-cart");
+  const cartItems = [getLocalStorage('so-cart')];
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
-  document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.querySelector('.product-list').innerHTML = htmlItems.join('');
 }
+
+
 
 function cartItemTemplate(item) {
   const newItem = `<li class="cart-card divider">
