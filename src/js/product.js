9e275a02c067918f8e4a05409a535c2b5d7d60fa
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-01-20 12:33:10 -0700

    class team activity 2 commit

--- src/js/product.js
+++ src/js/product.js
@@ -1,7 +1,11 @@
-import { setLocalStorage } from './utils.mjs';
 import ProductData from './ProductData.mjs';
+import { setLocalStorage, getParam } from './utils.mjs';
 
 const dataSource = new ProductData('tents');
+
+const productId = getParam('product');
+
+console.log(dataSource.findProductById(productId));
 
 function addProductToCart(product) {
   setLocalStorage('so-cart', product);
