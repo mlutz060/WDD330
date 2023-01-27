ee8f69abec75cdd963abd97c95bec998496a65cc
Brandon Lisonbee <blisonbee213596@gmail.com>
2023-01-27 13:04:52 -0700

    team activity 3 commit

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -25,6 +25,20 @@ export function setClick(selector, callback) {
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  const product = urlParams.get('product');
+  const product = urlParams.get(param);
   return product;
+}
+
+export function renderListWithTemplate(
+  templateFn,
+  parentElement,
+  list,
+  position = "afterbegin",
+  clear = false
+) {
+  const htmlStrings = list.map(templateFn);
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
