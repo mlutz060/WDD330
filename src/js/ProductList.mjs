9a712ed06df0c3105cad86f98694bd15f5ab8054
mlutz06 <madison.lutz@sentinelofsafety.com>
2023-02-21 11:42:21 -0700

    login

--- src/js/ProductList.mjs
+++ src/js/ProductList.mjs
@@ -8,7 +8,7 @@ function productCardTemplate(product) {
     <img src="${product.Images.PrimaryMedium}" alt="Image of ${product.Name}"/>
     <h3 class="card__brand">${product.Brand.Name}</h3>
     <h2 class="card__name">${product.Name}</h2>
-    <p class="product-card__price">$${product.FinalPrice}</p></a>
+    <p class="product-card__price">${product.FinalPrice}</p></a>
   </li>`
 }
 
